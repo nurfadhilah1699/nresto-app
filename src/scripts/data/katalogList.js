c2219dc31e1f9843c4953f710ef4ca73694a28d8
Nurfadhilah <nurfadhilahalang480@gmail.com>
2021-11-07 14:15:56 +0800

    update aksesibilitas

--- src/scripts/data/katalogList.js
+++ src/scripts/data/katalogList.js
@@ -9,7 +9,7 @@ import ('../../DATA.json').then(({default: katalogJson}) => {
             <div class="location" tabindex="0">${katalog['city']}</div>
             <div class="katalogItemContent">
                 <p class="rating">Rating : 
-                    <a href="#" class="ratingNumber" title="Rating Restoran">${katalog['rating']}</a>
+                    <span class="ratingNumber">${katalog['rating']}</span>
                 </p>
                 <h1 class="katalogItemName"><a href="#">${katalog['name']}</a></h1>
                 <div class="katalogItemDescription" tabindex="0">${katalog['description'].slice(0, 300)}...</div>
