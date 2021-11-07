ec414421d2eaaca5fcb99c02143db6fee603e145
Nurfadhilah <nurfadhilahalang480@gmail.com>
2021-11-07 11:55:54 +0800

    updated aksesibilitas

--- src/scripts/data/katalogList.js
+++ src/scripts/data/katalogList.js
@@ -6,13 +6,13 @@ import ('../../DATA.json').then(({default: katalogJson}) => {
         dataKatalog +=`
         <article class="katalogItem">
             <img class="katalogItemThumbnail" src="${katalog['pictureId']}" alt="${katalog['name']}" title="${katalog['name']}">
-            <div class="location">${katalog['city']}</div>
+            <div class="location" tabindex="0">${katalog['city']}</div>
             <div class="katalogItemContent">
                 <p class="rating">Rating : 
-                    <a href="#" class="ratingNumber">${katalog['rating']}</a>
+                    <a href="#" class="ratingNumber" title="Rating Restoran">${katalog['rating']}</a>
                 </p>
                 <h1 class="katalogItemName"><a href="#">${katalog['name']}</a></h1>
-                <div class="katalogItemDescription">${katalog['description'].slice(0, 300)}...</div>
+                <div class="katalogItemDescription" tabindex="0">${katalog['description'].slice(0, 300)}...</div>
             </div>
         </article>
         `;
